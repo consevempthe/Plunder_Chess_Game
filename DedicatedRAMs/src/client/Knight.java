90df67bea7cb982fcc02a2cb20fb4bdb90f2b119
EAL <liem.ethan@gmail.com>
2020-10-05 12:07:02 -0600

    added Knight and tests

---
+++ DedicatedRAMs/src/client/Knight.java
@@ -0,0 +1,54 @@
+package client;
+
+import java.util.ArrayList;
+
+public class Knight extends ChessPiece {
+
+
+    public Knight(ChessBoard board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public String toString() {
+        return this.getColor() == Color.WHITE ? "\u2658" : "\u265E";
+    }
+
+    @Override
+    public ArrayList<String> legalMoves() {
+        ArrayList<String> moves = new ArrayList<>();
+
+        //this is the possible moves that a Knight can make
+        int[][] moveSet = {
+                {1, -2}, {2, -1}, {2, 1}, {1, 2},
+                {-1, -2}, {-2, -1}, {-2, 1}, {-1, 2}
+        };
+
+        //loop thru the moveSet by adding the moveSet to the pieces position and make sure that it is a legal move.
+        for (int[] m : moveSet) {
+            char row = (char) ('1' + this.row + m[0]);
+            char col = (char) ('a' + this.column + m[1]);
+
+            String newPosition = col + "" + row;
+            /*this loop determines if a position is legal, legality is assuring that the space is either null/empty
+              or the space is occupied by an enemy piece.
+            */
+            try {
+                if(this.board.isPositionOnBoard(newPosition)) {
+
+                    if(this.board.getPiece(newPosition) != null) {
+                        if(this.board.getPiece(newPosition).getColor() != this.getColor())
+                            moves.add(newPosition);
+                    } else {
+                        moves.add(newPosition);
+                    }
+                }
+            } catch(IllegalPositionException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        return moves;
+    }
+}
---
+++ DedicatedRAMs/src/client/KnightTest.java
@@ -0,0 +1,67 @@
+package client;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KnightTest {
+
+    ChessBoard chess = new ChessBoard();
+    Knight k_w;
+    Knight k_b;
+
+    @BeforeEach
+    void setup() {
+        k_w = new Knight(chess, ChessPiece.Color.WHITE);
+        k_b = new Knight(chess, ChessPiece.Color.BLACK);
+    }
+
+    @Test
+    void testToString() {
+        assertEquals("\u2658", k_w.toString());
+        assertEquals("\u265E", k_b.toString());
+    }
+
+    @Test
+    void legalMoves() {
+        chess.placePiece(k_w, "b1");
+        assertEquals(3, k_w.legalMoves().size());
+        assertTrue(k_w.legalMoves().contains("a3"));
+        assertTrue(k_w.legalMoves().contains("c3"));
+        assertTrue(k_w.legalMoves().contains("d2"));
+
+        chess.placePiece(k_b, "b8");
+        assertEquals(3, k_b.legalMoves().size());
+        assertTrue(k_b.legalMoves().contains("a6"));
+        assertTrue(k_b.legalMoves().contains("c6"));
+        assertTrue(k_b.legalMoves().contains("d7"));
+
+        chess.placePiece(k_w, "h1");
+        assertEquals(2, k_w.legalMoves().size());
+    }
+
+    @Test
+    void noLegalMoves() {
+        chess.placePiece(k_w, "c4");
+        chess.placePiece(new Knight(chess, ChessPiece.Color.WHITE), "b6");
+        chess.placePiece(new Knight(chess, ChessPiece.Color.WHITE), "d6");
+        chess.placePiece(new Knight(chess, ChessPiece.Color.WHITE), "a5");
+        chess.placePiece(new Knight(chess, ChessPiece.Color.WHITE), "e5");
+        chess.placePiece(new Knight(chess, ChessPiece.Color.WHITE), "b2");
+        chess.placePiece(new Knight(chess, ChessPiece.Color.WHITE), "b2");
+        chess.placePiece(new Knight(chess, ChessPiece.Color.WHITE), "a3");
+        chess.placePiece(new Knight(chess, ChessPiece.Color.WHITE), "e3");
+
+        assertEquals(0, k_w.legalMoves().size());
+
+    }
+
+    @Test
+    void legalCaptureMoves() {
+        chess.placePiece(k_w, "g1");
+        chess.placePiece(k_b, "f3");
+        assertEquals(3, k_w.legalMoves().size());
+        assertTrue(k_w.legalMoves().contains("f3"));
+    }
+}
