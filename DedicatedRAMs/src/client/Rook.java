90b4c9b21e58ef4022d979fe3f8afd8ba6629767
axel <axel@10.0.0.98>
2020-10-05 12:21:09 -0600

    Initial Commit part 2

--- DedicatedRAMs/src/client/Rook.java
+++ DedicatedRAMs/src/client/Rook.java
@@ -1,5 +1,26 @@
 package client;
 
-public class Rook {
+import java.util.ArrayList;
+
+public class Rook extends ChessPiece {
+
+	public Rook(ChessBoard board, Color color) {
+		super(board, color);
+	}
+
+	@Override
+	public String toString() {
+		if (this.color == ChessPiece.Color.WHITE) {
+			return "\u2656";
+		} else {
+			return "\u265C";
+		}
+	}
+
+	@Override
+	public ArrayList<String> legalMoves() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
