d93b3ca54551783ed274b41f25ca31c1b5c58e91
Faour <najy.faour@hp.com>
2020-10-08 11:57:30 -0600

    Move History added with tests and documentation.

--- DedicatedRAMs/src/client/MoveHistory.java
+++ DedicatedRAMs/src/client/MoveHistory.java
@@ -2,24 +2,44 @@ package client;
 
 import java.util.ArrayList;
 
+/**
+ * MoveHistory class is a class containing the moves of a game in an ArrayList. The moveHistory list should be added to as players make moves.
+ * @author DedicatedRAMs Team
+ *
+ */
 public class MoveHistory {
 
 	private ArrayList<Move> moveHistory;
 	
+	/**
+	 * Default Constructor for MoveHistory creates an empty list of Moves.
+	 */
 	public MoveHistory() {
 		this.moveHistory = new ArrayList<Move>();
 	}
 	
+	/**
+	 * getMoveHistory() is the getter for the moveHistory arrayList so that we can view the lists content outside of the MoveHistory class.
+	 * @return ArrayList<Move>
+	 */
 	public ArrayList<Move> getMoveHistory() {
 		return moveHistory;
 	}
 
+	/**
+	 * setMoveHistory() is the setter for the moveHistory ArrayList so that we can set the list if the game is being resumed, potentially. Check later to see if necessary.
+	 * @param moveHistory - the list of moves in a game.
+	 */
 	public void setMoveHistory(ArrayList<Move> moveHistory) {
 		this.moveHistory = moveHistory;
 	}
 
+	/**
+	 * addMoveToMoveHistory() adds a Move to the end of the ArrayList as a game progesses.
+	 * @param moveToAdd - the move to add to the move history.
+	 */
 	public void addMoveToMoveHistory(Move moveToAdd) {
-		
+		moveHistory.add(moveToAdd);
 	}
 	
 }
